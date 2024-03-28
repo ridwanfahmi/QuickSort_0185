ab579f9de15bdfb80f2f65c067d4a4ea982a0ccd
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-03-28 11:00:01 +0700

    membuat prosedur display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -87,8 +87,25 @@ void q_short(int low, int high)
 
 
     q_short(j + 1, high);
+
+
 }
 
+
+
+void display() {
+    cout << "\n-----------" << endl;
+    cout << "Shorted Array" << endl;
+    cout << "-----------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
 
 int main()
 {
